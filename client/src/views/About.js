5bc47877f0ae66a517b23d23bcebc036fe125f9b
anhng <anananh1999@gmail.com>
2024-11-21 13:30:37 +0700

    Add About info

--- client/src/views/About.js
+++ client/src/views/About.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import NavBarMenu from '../components/layout/NavBarMenu';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Accordion from 'react-bootstrap/Accordion';
+
 const About = () => {
   return (
     <div>
       <NavBarMenu>
-      
+        <Container>
+          <Row>
+            <Col xs={6} md={4}>
+              <Image src="holder.js/171x180" rounded />
+            </Col>
+            <Col>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Accordion Item #1</Accordion.Header>
+                  <Accordion.Body>
+                  PTE Magic Technical Support: support@ptemagic.com
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
       </NavBarMenu>
       About
     </div>
-    
-  )
+  );
 }
 
 export default About
